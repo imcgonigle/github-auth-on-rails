1ba831066dd074ba74c33b47e8702ea78733359a
Ian McGonigle <ian.mcgonigle@oddball.io>
2021-10-22 13:02:18 -0600

    Run rails g controller dashboard index
    I also made the Dashboard controller protect by adding
    before_action :authenticate_user!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
-  get 'home/index'
+  
   root to: "home#index"
+  
+  get 'home/index'
+  get 'dashboard/index'
 end
